3c44351e9821e3bb0c1db5bf4b7f0d85ded90898
Your Name <you@example.com>
2023-10-21 03:20:31 +0200

    Add project files.

---
+++ TryParseConversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TryParseConversion
+{//Msimango KSI
+    class Program
+    { /* A C# program to convert a string value to integer using a TryParse method.
+       If the string value is a text, it will skip the first statement to the else statement.
+      If the value is a number,it will output the result*/
+        static void Main(string[] args)
+        {
+            string value = "100";
+            int result = 0;
+            if (int.TryParse(value, out result))
+            {
+                Console.WriteLine($"Result: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Unable to find measurement");
+            }
+            if (result > 0)
+            {
+                Console.WriteLine($"The measurement report is {202 + result}");
+            }
+        }
+        
+    }
+}
